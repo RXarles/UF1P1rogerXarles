aef7274001f5886d2898240c03c4c29fb044929b
Roger <rxarles@cendrassos.net>
2020-09-29 15:47:31 +0200

    e1p2

--- index.php
+++ index.php
@@ -10,8 +10,19 @@
     <link rel="stylesheet" href="index.css">
 
     <title>Com a casa enlloc!</title>
+
+    <?php
+    $images = array(
+      1=> 'https://cdn.pixabay.com/photo/2020/09/11/00/11/landscape-5561678_960_720.jpg',
+      2=> 'https://cdn.pixabay.com/photo/2017/01/20/00/30/maldives-1993704_960_720.jpg',
+      3=> 'https://cdn.pixabay.com/photo/2016/02/19/10/10/city-1209105_960_720.jpg',
+      4=> 'https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547_960_720.jpg'
+    );
+    ?>
   </head>
-  <body>
+  <body <?php echo(' style ="background-image: url('.$images[3].')"')?>>
+
+    
     <h1 class="text-center" >Com a casa enlloc!</h1>
 
 
@@ -20,13 +31,15 @@
             <div class="col-8">
                 <div class="container">
                     <div class="row row-cols-3">
-                        <div class="col"><div class="m-3 p-2 bg-dark text-white">Enllaç 1</div></div>
-                        <div class="col"><div class="m-3 p-2 bg-dark text-white">Enllaç 2</div></div>
-                        <div class="col"><div class="m-3 p-2 bg-dark text-white">Enllaç 3</div></div>
-                        <div class="col"><div class="m-3 p-2 bg-dark text-white">Enllaç 4</div></div>
-                        <div class="col"><div class="m-3 p-2 bg-dark text-white">Enllaç 5</div></div>
-                        <div class="col"><div class="m-3 p-2 bg-dark text-white">Enllaç 6</div></div>
+                      <?php
 
+                      $numEnllacos = 7;
+
+                      for($i = 1;$i<=$numEnllacos;$i++)
+                      {
+                        echo('<div class="col"><div class="m-3 p-2 bg-dark text-white">Enllaç '.$i.' </div></div>'); 
+                      }
+                      ?>
                     </div>
                 </div>
             </div>
@@ -35,6 +48,8 @@
     </div>
 
     <!-- Optional JavaScript -->
+
+
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
